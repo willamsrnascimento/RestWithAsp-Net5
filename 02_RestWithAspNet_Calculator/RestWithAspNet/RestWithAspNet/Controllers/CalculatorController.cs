f77b816299cb6e4346504f3222ca380e18362217
Willams Reis <willams.trb@gmail.com>
2023-02-15 17:25:36 -0300

    Implementing Calculator

---
+++ 02_RestWithAspNet_Calculator/RestWithAspNet/RestWithAspNet/Controllers/CalculatorController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithAspNet.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondtNumber}")]
+        public IActionResult GetSum(string firstNumber, string secondtNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondtNumber))
+            {
+                var sum = ConvertToNumeric(firstNumber) + ConvertToNumeric(secondtNumber);
+
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        private bool IsNumeric(string strNumber)
+        {
+            double number;
+            bool isNumeric = double.TryParse(strNumber, System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out number);
+
+            return isNumeric;
+        }
+        private decimal ConvertToNumeric(string strNumber)
+        {
+            decimal decimalValue;
+
+            if(decimal.TryParse(strNumber, out decimalValue))
+            {
+                return decimalValue;
+            }
+            return 0;   
+        }
+
+
+    }
+}
